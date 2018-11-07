3e7fbb94e6cf5400b1f94bc745a9fcc4f4208de9
Ying Dong <dongyingname@yahoo.com>
2018-11-06 18:27:56 -0700

    The tweeter section is constructed. Starting to build dynamic tweets.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,24 @@
+//Document Ready Check
+
+//Dynamic change to textarea
+function changeCounter() {
+    $("#text")[0].addEventListener('keydown', function () {
+        const maxLength = 140;
+        let charRemain = maxLength - this.value.length;
+        $("#counter")[0].textContent = charRemain;
+       
+        if (charRemain < 0) {
+            $("#counter")[0].style.color = 'red';
+        } else {
+            $("#counter")[0].style.color = '';
+        }
+    });
+}
+
 $(document).ready(function () {
+
     console.log('document is ready');
 
-    const maxLength = 140;
-    
-    $("#text")[0].addEventListener('keydown', function () {
-        let charRemain = maxLength - this.value.length;
-        console.log('keydowned');
-        console.log(charRemain);
-        $("#counter")[0].textContent = charRemain;
-
-    });
-
+    changeCounter();
 
 });
-
