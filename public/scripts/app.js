25911fedbfea22ca58cebf771613293cf389cb4d
Ying Dong <dongyingname@yahoo.com>
2018-11-07 19:56:21 -0700

    Add comments to index.html, app.js, composer-char-counter.js, and layout.css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,9 @@
+//tweeter constructor. It dnamicly reconstruct the tweets so
+//that they look the same
+//I manually hardcode the layout, but the css files automatic 
+//apply the styles to respective classes
 function createTweetElement(data) {
+    // create elements with special classnames
     const $tweet = $("<article>").addClass("article tweet");
     const $header = $("<header>").addClass("tweetHeader");
     const $image = $("<img>").addClass("imgHeader");
@@ -9,31 +14,52 @@ function createTweetElement(data) {
     const $span4 = $("<span>").addClass("span day");
     const $span5 = $("<span>").addClass("span link");
     $image.attr('src', data.user.avatars.small);
-    $span1.text(data.user.name); //not working
+    $span1.text(data.user.name); 
     $span2.text(data.user.handle);
     $span3.text(data.content.text);
+    //uses newDate() to convert a unix standard date format
+    //to a real date
     const date = new Date(data.created_at).toString();
     const arr = date.split(' ');
+    //index 0 1 2 3 refers to day month year 
     const newDate = arr[0] + arr[1] + arr[2] + arr[3];
     $span4.text(newDate);
+    //reconstruct big sections by appending children in the disired
+    //and logical order
     $header.append($image, $span1, $span2);
     $footer.append($span4, $span5);
     $tweet.append($header, $span3, $footer);
+
     return $tweet;
 }
 
+//renderTweets append the newly constructed main sub section to the 
+//existing main, where data should be an object in a manner such that
+//allows the createTweetElement function to work on
 function renderTweets(data) {
     const $tweet = createTweetElement(data);
     $('main').append($tweet);
 }
 
+//this function:
+//1. add an event listener using jquery syntax by listening submit action
+//on <form> element
+//2. if statements to give a dynamic alert using slideToggle on a span element 
+//in the <form> element whenever the character length in the textarea is over
+//140 and below 0
+// 3. the error messages would disapear when additional input is detected
+
+// 4. if the character length  is between 0 and 140, it will console.log and send 
+// a post request to the server through ajax
+// 5. loadTweets function is the value of the complete key so that after the post
+// it will trigger and append the new tweet to the existing tweets
 function createSubmitHandler(callback) {
     $('form').submit(function (event) {
-
         event.preventDefault();
         const form = $(this);
         const formData = form.serialize();
         const text = $('textarea').val();
+
         if (!text.length) {
             return $("span.low").slideToggle('slow');
 
@@ -54,6 +80,10 @@ function createSubmitHandler(callback) {
     });
 }
 
+// 1. tweetData: manually construct the tweet HTML structure that take 
+// the data from server to fill the key values
+// 2. callback the renderTweets function and pass tweetData into it
+// so to append a new tweet to the exisiting tweet
 function loadTweets() {
     const tweetData = {
         "user": {
@@ -69,6 +99,9 @@ function loadTweets() {
     renderTweets(tweetData);
 }
 
+//a event listener on to #composer element 
+//when being clicked, the .section element will be toggled visible
+//the textarea is focused asynchronously
 function clickComposer() {
     $('#composer').click(function (event) {
         $(".section").slideToggle('slow', function () {
@@ -76,6 +109,8 @@ function clickComposer() {
         });
     })
 }
+
+//call of all the above function after the document DOM is loaded
 $(document).ready(function () {
     createSubmitHandler();
     clickComposer();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,13 @@
 //Document Ready Check
 
 //Dynamic change to textarea
+// 1. listen to keydown event to textarea 
+// 2. the max character length in textarea is 140 allowed
+// 3. the if statements make sure the color of the counter number becomes
+// red when falls below 0 and normal when rise above 0
+//
 function changeCounter() {
-    $("#text")[0].addEventListener('keydown', function () {
+    $("#text").keydown(function () {
         const maxLength = 140;
         let charRemain = maxLength - this.value.length;
         $("#counter")[0].textContent = charRemain;
@@ -13,7 +18,7 @@ function changeCounter() {
         }
     });
 }
-
+//start to use changeCounter after DOM is loaded
 $(document).ready(function () {
 
     changeCounter();
