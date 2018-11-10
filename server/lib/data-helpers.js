c765e4dcf4ded596228c543995cfe9e009071c79
Ying Dong <dongyingname@yahoo.com>
2018-11-09 17:48:21 -0700

    db.collection.insert() in function saveTweet. db.collection.find() in function getTweets. the server is able to get db from mongo database. However, the newly created tweet object need to be refomatted so it follow the database contect format

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,28 +1,34 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+//const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        //console.log('db.tweets', db.tweets)
-        callback(null, true);
-      });
+    saveTweet: function (newTweet, callback) {
+      db.collection("tweets").insert(newTweet);
+      // db.tweets[0].push(newTweet);
+      // console.log(newTweet);
+      // console.log('db.tweets', db);
+      callback(null, true);
+
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
+    getTweets: function (callback) {
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) throw err;
+
+        // let obj = tweets[0].tweets;
+        // console.log(obj);
+        // console.log("Logging each tweet:");;
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, tweets[0].tweets.sort(sortNewestFirst));
+
       });
     }
-
-  };
-}
+  }
+};
